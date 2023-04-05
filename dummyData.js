ac50190ac5ea64189702326ead1ff422222344e7
mason <masonpr1@gmail.com>
2023-04-05 10:06:54 -0500

    add dummy data

---
+++ dummyData.js
@@ -0,0 +1,322 @@
+[
+  {
+    created: '2023-02-25T00:01:47Z',
+    category: {
+      label: 'IT Jobs',
+      __CLASS__: 'Adzuna::API::Response::Category',
+      tag: 'it-jobs',
+    },
+    salary_is_predicted: '1',
+    salary_max: 63810.44,
+    latitude: 40.07424,
+    longitude: -74.82318,
+    description:
+      "Job ID: 625612BR Date posted: Jan. 10, 2023 Locations: Moorestown, New Jersey Description: By applying for this role you will be considered for enterprise-wide opportunities within Lockheed Martin for the same or similar skill set. Step into the challenge of engineering a better tomorrow. You'll be backed by the inspiration of Lockheed Martin's culture, based on a heritage of innovation and creativity, serving the needs of our nation and humanity. Here you can make a difference in many ways, fr…",
+    location: {
+      display_name: 'Burlington City, Burlington County',
+      area: ['US', 'New Jersey', 'Burlington County', 'Burlington City'],
+      __CLASS__: 'Adzuna::API::Response::Location',
+    },
+    id: '3943007763',
+    redirect_url:
+      'https://www.adzuna.com/land/ad/3943007763?se=0pjtaWLS7RGHMVQQ_9x0yQ&utm_medium=api&utm_source=d4457f4f&v=9E81E4A10AEFDCC5605A5123175E9EB10B1525C1',
+    company: {
+      __CLASS__: 'Adzuna::API::Response::Company',
+      display_name: 'Lockheed Martin',
+    },
+    title: 'Software Engineer',
+    contract_time: 'full_time',
+    __CLASS__: 'Adzuna::API::Response::Job',
+    adref:
+      'eyJhbGciOiJIUzI1NiJ9.eyJzIjoiMHBqdGFXTFM3UkdITVZRUV85eDB5USIsImkiOiIzOTQzMDA3NzYzIn0.5YBMtNQ57rhWLPrCTbygqLWwTEVAzJ8zK0FdpD-MtlQ',
+    salary_min: 63810.44,
+  },
+  {
+    location: {
+      display_name: 'Deptford, Gloucester County',
+      area: ['US', 'New Jersey', 'Gloucester County', 'Deptford'],
+      __CLASS__: 'Adzuna::API::Response::Location',
+    },
+    description:
+      "Job ID: 625612BR Date posted: Jan. 10, 2023 Locations: Moorestown, New Jersey Description: By applying for this role you will be considered for enterprise-wide opportunities within Lockheed Martin for the same or similar skill set. Step into the challenge of engineering a better tomorrow. You'll be backed by the inspiration of Lockheed Martin's culture, based on a heritage of innovation and creativity, serving the needs of our nation and humanity. Here you can make a difference in many ways, fr…",
+    longitude: -75.130176,
+    latitude: 39.823294,
+    salary_max: 72986.55,
+    salary_is_predicted: '1',
+    category: {
+      tag: 'it-jobs',
+      __CLASS__: 'Adzuna::API::Response::Category',
+      label: 'IT Jobs',
+    },
+    created: '2023-02-25T00:01:47Z',
+    contract_time: 'full_time',
+    title: 'Software Engineer',
+    company: {
+      __CLASS__: 'Adzuna::API::Response::Company',
+      display_name: 'Lockheed Martin',
+    },
+    redirect_url:
+      'https://www.adzuna.com/land/ad/3943007765?se=0pjtaWLS7RGHMVQQ_9x0yQ&utm_medium=api&utm_source=d4457f4f&v=EDAC690B26AD60C61508F6EBE84995B71B41C910',
+    id: '3943007765',
+    salary_min: 72986.55,
+    adref:
+      'eyJhbGciOiJIUzI1NiJ9.eyJzIjoiMHBqdGFXTFM3UkdITVZRUV85eDB5USIsImkiOiIzOTQzMDA3NzY1In0.t1LEFK1PLrPWd7bBw_f3czLqkDMKd2UoKcWK6akJnH0',
+    __CLASS__: 'Adzuna::API::Response::Job',
+  },
+  {
+    location: {
+      __CLASS__: 'Adzuna::API::Response::Location',
+      area: ['US', 'New Jersey', 'Burlington County', 'Moorestown'],
+      display_name: 'Moorestown, Burlington County',
+    },
+    latitude: 39.97872,
+    longitude: -74.93886,
+    description:
+      "Job ID: 625612BR Date posted: Jan. 10, 2023 Locations: Moorestown, New Jersey Description: By applying for this role you will be considered for enterprise-wide opportunities within Lockheed Martin for the same or similar skill set. Step into the challenge of engineering a better tomorrow. You'll be backed by the inspiration of Lockheed Martin's culture, based on a heritage of innovation and creativity, serving the needs of our nation and humanity. Here you can make a difference in many ways, fr…",
+    salary_max: 62645.2,
+    created: '2023-01-17T20:43:07Z',
+    category: {
+      tag: 'it-jobs',
+      __CLASS__: 'Adzuna::API::Response::Category',
+      label: 'IT Jobs',
+    },
+    salary_is_predicted: '1',
+    contract_time: 'full_time',
+    title: 'Software Engineer',
+    company: {
+      display_name: 'Lockheed Martin',
+      __CLASS__: 'Adzuna::API::Response::Company',
+    },
+    id: '3855858393',
+    redirect_url:
+      'https://www.adzuna.com/land/ad/3855858393?se=0pjtaWLS7RGHMVQQ_9x0yQ&utm_medium=api&utm_source=d4457f4f&v=AC7C2CE9BCEA08161A29350E2CB864AD65A4715E',
+    salary_min: 62645.2,
+    adref:
+      'eyJhbGciOiJIUzI1NiJ9.eyJzIjoiMHBqdGFXTFM3UkdITVZRUV85eDB5USIsImkiOiIzODU1ODU4MzkzIn0.9O9gnoByja--cUSc4ua4PokX4t7_z4pY7v7JNojxiE8',
+    __CLASS__: 'Adzuna::API::Response::Job',
+  },
+  {
+    contract_time: 'full_time',
+    redirect_url:
+      'https://www.adzuna.com/land/ad/3943007767?se=0pjtaWLS7RGHMVQQ_9x0yQ&utm_medium=api&utm_source=d4457f4f&v=DD0A70725C0D19308915B705C55E2ED8847D12C2',
+    id: '3943007767',
+    title: 'Software Engineer',
+    company: {
+      display_name: 'Lockheed Martin',
+      __CLASS__: 'Adzuna::API::Response::Company',
+    },
+    adref:
+      'eyJhbGciOiJIUzI1NiJ9.eyJpIjoiMzk0MzAwNzc2NyIsInMiOiIwcGp0YVdMUzdSR0hNVlFRXzl4MHlRIn0.K-R3lJabKXmCQDkbA5R6NZa-9n8FIZN35mzhcJNuciw',
+    salary_min: 58151.84,
+    __CLASS__: 'Adzuna::API::Response::Job',
+    description:
+      "Job ID: 625612BR Date posted: Jan. 10, 2023 Locations: Moorestown, New Jersey Description: By applying for this role you will be considered for enterprise-wide opportunities within Lockheed Martin for the same or similar skill set. Step into the challenge of engineering a better tomorrow. You'll be backed by the inspiration of Lockheed Martin's culture, based on a heritage of innovation and creativity, serving the needs of our nation and humanity. Here you can make a difference in many ways, fr…",
+    longitude: -74.81112,
+    latitude: 39.8619,
+    location: {
+      display_name: 'Medford Lakes, Burlington County',
+      area: ['US', 'New Jersey', 'Burlington County', 'Medford Lakes'],
+      __CLASS__: 'Adzuna::API::Response::Location',
+    },
+    salary_is_predicted: '1',
+    category: {
+      __CLASS__: 'Adzuna::API::Response::Category',
+      label: 'IT Jobs',
+      tag: 'it-jobs',
+    },
+    created: '2023-02-25T00:01:47Z',
+    salary_max: 58151.84,
+  },
+  {
+    __CLASS__: 'Adzuna::API::Response::Job',
+    salary_min: 67814.08,
+    adref:
+      'eyJhbGciOiJIUzI1NiJ9.eyJzIjoiMHBqdGFXTFM3UkdITVZRUV85eDB5USIsImkiOiIzOTQzMDA3NzYyIn0.bcFzgIinLAJe_fyqYzZ7amv8LXF2FWceLTv4ir1_Flg',
+    title: 'Software Engineer',
+    company: {
+      __CLASS__: 'Adzuna::API::Response::Company',
+      display_name: 'Lockheed Martin',
+    },
+    id: '3943007762',
+    redirect_url:
+      'https://www.adzuna.com/land/ad/3943007762?se=0pjtaWLS7RGHMVQQ_9x0yQ&utm_medium=api&utm_source=d4457f4f&v=731BA837CCFF8EF0E04ABF965589F3F4F727D6A0',
+    contract_time: 'full_time',
+    salary_max: 67814.08,
+    created: '2023-02-25T00:01:47Z',
+    category: {
+      tag: 'it-jobs',
+      __CLASS__: 'Adzuna::API::Response::Category',
+      label: 'IT Jobs',
+    },
+    salary_is_predicted: '1',
+    location: {
+      area: ['US', 'New Jersey', 'Burlington County', 'Mount Laurel Township'],
+      __CLASS__: 'Adzuna::API::Response::Location',
+      display_name: 'Mount Laurel Township, Burlington County',
+    },
+    longitude: -74.899646,
+    latitude: 39.94833,
+    description:
+      "Job ID: 625612BR Date posted: Jan. 10, 2023 Locations: Moorestown, New Jersey Description: By applying for this role you will be considered for enterprise-wide opportunities within Lockheed Martin for the same or similar skill set. Step into the challenge of engineering a better tomorrow. You'll be backed by the inspiration of Lockheed Martin's culture, based on a heritage of innovation and creativity, serving the needs of our nation and humanity. Here you can make a difference in many ways, fr…",
+  },
+  {
+    __CLASS__: 'Adzuna::API::Response::Job',
+    salary_min: 64917.44,
+    adref:
+      'eyJhbGciOiJIUzI1NiJ9.eyJzIjoiMHBqdGFXTFM3UkdITVZRUV85eDB5USIsImkiOiIzOTQzMDA3NzY2In0.HY03hNThjiw9_6JiP9dHtEqi0prn28Bk7ex7YyLIYE8',
+    company: {
+      display_name: 'Lockheed Martin',
+      __CLASS__: 'Adzuna::API::Response::Company',
+    },
+    title: 'Software Engineer',
+    redirect_url:
+      'https://www.adzuna.com/land/ad/3943007766?se=0pjtaWLS7RGHMVQQ_9x0yQ&utm_medium=api&utm_source=d4457f4f&v=B248EBDF7C99AEDFBA48F98683538734B27E8E4E',
+    id: '3943007766',
+    contract_time: 'full_time',
+    salary_max: 64917.44,
+    salary_is_predicted: '1',
+    category: {
+      tag: 'it-jobs',
+      label: 'IT Jobs',
+      __CLASS__: 'Adzuna::API::Response::Category',
+    },
+    created: '2023-02-25T00:01:47Z',
+    location: {
+      area: ['US', 'New Jersey', 'Burlington County', 'Tabernacle'],
+      __CLASS__: 'Adzuna::API::Response::Location',
+      display_name: 'Tabernacle, Burlington County',
+    },
+    description:
+      "Job ID: 625612BR Date posted: Jan. 10, 2023 Locations: Moorestown, New Jersey Description: By applying for this role you will be considered for enterprise-wide opportunities within Lockheed Martin for the same or similar skill set. Step into the challenge of engineering a better tomorrow. You'll be backed by the inspiration of Lockheed Martin's culture, based on a heritage of innovation and creativity, serving the needs of our nation and humanity. Here you can make a difference in many ways, fr…",
+    latitude: 39.843385,
+    longitude: -74.710705,
+  },
+  {
+    latitude: 39.927429,
+    longitude: -74.726579,
+    description:
+      "Job ID: 625612BR Date posted: Jan. 10, 2023 Locations: Moorestown, New Jersey Description: By applying for this role you will be considered for enterprise-wide opportunities within Lockheed Martin for the same or similar skill set. Step into the challenge of engineering a better tomorrow. You'll be backed by the inspiration of Lockheed Martin's culture, based on a heritage of innovation and creativity, serving the needs of our nation and humanity. Here you can make a difference in many ways, fr…",
+    location: {
+      __CLASS__: 'Adzuna::API::Response::Location',
+      area: ['US', 'New Jersey', 'Burlington County', 'Southampton'],
+      display_name: 'Southampton, Burlington County',
+    },
+    created: '2023-02-25T00:01:47Z',
+    category: {
+      label: 'IT Jobs',
+      __CLASS__: 'Adzuna::API::Response::Category',
+      tag: 'it-jobs',
+    },
+    salary_is_predicted: '1',
+    salary_max: 65137.16,
+    adref:
+      'eyJhbGciOiJIUzI1NiJ9.eyJzIjoiMHBqdGFXTFM3UkdITVZRUV85eDB5USIsImkiOiIzOTQzMDA3NzYxIn0.cfmHdT6OlrPUlLbvkOCnp5E5x1ukhdemJDMD2lccXf4',
+    salary_min: 65137.16,
+    __CLASS__: 'Adzuna::API::Response::Job',
+    contract_time: 'full_time',
+    id: '3943007761',
+    redirect_url:
+      'https://www.adzuna.com/land/ad/3943007761?se=0pjtaWLS7RGHMVQQ_9x0yQ&utm_medium=api&utm_source=d4457f4f&v=0AE1BF63DE39CDD1992844F4B43AA8189B3F6E40',
+    company: {
+      __CLASS__: 'Adzuna::API::Response::Company',
+      display_name: 'Lockheed Martin',
+    },
+    title: 'Software Engineer',
+  },
+  {
+    salary_min: 64356.13,
+    adref:
+      'eyJhbGciOiJIUzI1NiJ9.eyJpIjoiMzg1NDg5NDQ2NSIsInMiOiIwcGp0YVdMUzdSR0hNVlFRXzl4MHlRIn0.RJkc_yKmRAMzrsTxnBLJSB3uM86bOZZpUsbVz0WVycU',
+    __CLASS__: 'Adzuna::API::Response::Job',
+    contract_time: 'full_time',
+    company: {
+      __CLASS__: 'Adzuna::API::Response::Company',
+      display_name: 'Lockheed Martin',
+    },
+    title: 'Software Engineer',
+    redirect_url:
+      'https://www.adzuna.com/land/ad/3854894465?se=0pjtaWLS7RGHMVQQ_9x0yQ&utm_medium=api&utm_source=d4457f4f&v=7186D0679065B8F26ECBDF0FF15E94F88E7A1B00',
+    id: '3854894465',
+    location: {
+      __CLASS__: 'Adzuna::API::Response::Location',
+      area: ['US', 'Pennsylvania', 'Montgomery County', 'Elkins Park'],
+      display_name: 'Elkins Park, Montgomery County',
+    },
+    description:
+      "Job ID: 625612BR Date posted: Jan. 10, 2023 Locations: Moorestown, New Jersey Description: By applying for this role you will be considered for enterprise-wide opportunities within Lockheed Martin for the same or similar skill set. Step into the challenge of engineering a better tomorrow. You'll be backed by the inspiration of Lockheed Martin's culture, based on a heritage of innovation and creativity, serving the needs of our nation and humanity. Here you can make a difference in many ways, fr…",
+    latitude: 40.0758,
+    longitude: -75.1277,
+    salary_max: 64356.13,
+    salary_is_predicted: '1',
+    created: '2023-01-17T13:01:17Z',
+    category: {
+      tag: 'it-jobs',
+      __CLASS__: 'Adzuna::API::Response::Category',
+      label: 'IT Jobs',
+    },
+  },
+  {
+    salary_max: 64072.28,
+    created: '2023-01-17T13:01:16Z',
+    category: {
+      tag: 'it-jobs',
+      __CLASS__: 'Adzuna::API::Response::Category',
+      label: 'IT Jobs',
+    },
+    salary_is_predicted: '1',
+    location: {
+      area: ['US', 'New Jersey', 'Camden County', 'Merchantville'],
+      __CLASS__: 'Adzuna::API::Response::Location',
+      display_name: 'Merchantville, Camden County',
+    },
+    latitude: 39.9532,
+    longitude: -75.0722,
+    description:
+      "Job ID: 625612BR Date posted: Jan. 10, 2023 Locations: Moorestown, New Jersey Description: By applying for this role you will be considered for enterprise-wide opportunities within Lockheed Martin for the same or similar skill set. Step into the challenge of engineering a better tomorrow. You'll be backed by the inspiration of Lockheed Martin's culture, based on a heritage of innovation and creativity, serving the needs of our nation and humanity. Here you can make a difference in many ways, fr…",
+    company: {
+      __CLASS__: 'Adzuna::API::Response::Company',
+      display_name: 'Lockheed Martin',
+    },
+    title: 'Software Engineer',
+    id: '3854894440',
+    redirect_url:
+      'https://www.adzuna.com/land/ad/3854894440?se=0pjtaWLS7RGHMVQQ_9x0yQ&utm_medium=api&utm_source=d4457f4f&v=2F88999858DA672D01C69C57B3722A6FFCD67756',
+    contract_time: 'full_time',
+    __CLASS__: 'Adzuna::API::Response::Job',
+    salary_min: 64072.28,
+    adref:
+      'eyJhbGciOiJIUzI1NiJ9.eyJzIjoiMHBqdGFXTFM3UkdITVZRUV85eDB5USIsImkiOiIzODU0ODk0NDQwIn0.k3Kkgqz8UG43xjQTtxo73ByXp-Z9K3xT_98aTQcZlwc',
+  },
+  {
+    contract_time: 'full_time',
+    id: '3943007768',
+    redirect_url:
+      'https://www.adzuna.com/land/ad/3943007768?se=0pjtaWLS7RGHMVQQ_9x0yQ&utm_medium=api&utm_source=d4457f4f&v=125EF1D42B84CE6413E92F2EE64864F186BB80BD',
+    title: 'Software Engineer',
+    company: {
+      __CLASS__: 'Adzuna::API::Response::Company',
+      display_name: 'Lockheed Martin',
+    },
+    adref:
+      'eyJhbGciOiJIUzI1NiJ9.eyJpIjoiMzk0MzAwNzc2OCIsInMiOiIwcGp0YVdMUzdSR0hNVlFRXzl4MHlRIn0.rMhTwu8Yq79MPRxCUUGvCNu5IQ9uQTg0woWK5f-Q7gU',
+    salary_min: 67215.16,
+    __CLASS__: 'Adzuna::API::Response::Job',
+    longitude: -74.9961,
+    latitude: 39.9023,
+    description:
+      "Job ID: 625612BR Date posted: Jan. 10, 2023 Locations: Moorestown, New Jersey Description: By applying for this role you will be considered for enterprise-wide opportunities within Lockheed Martin for the same or similar skill set. Step into the challenge of engineering a better tomorrow. You'll be backed by the inspiration of Lockheed Martin's culture, based on a heritage of innovation and creativity, serving the needs of our nation and humanity. Here you can make a difference in many ways, fr…",
+    location: {
+      display_name: 'Ellisburg, Camden County',
+      __CLASS__: 'Adzuna::API::Response::Location',
+      area: ['US', 'New Jersey', 'Camden County', 'Ellisburg'],
+    },
+    created: '2023-02-25T00:01:47Z',
+    category: {
+      label: 'IT Jobs',
+      __CLASS__: 'Adzuna::API::Response::Category',
+      tag: 'it-jobs',
+    },
+    salary_is_predicted: '1',
+    salary_max: 67215.16,
+  },
+];
