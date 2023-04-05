1c6d7b59aac0752049232f7513406dc12ae2236c
garrett hutson <hutsongarrett@gmail.com>
2023-04-05 11:09:17 -0400

    styles

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -5,12 +5,13 @@ import Link from 'next/link'
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between items-center h-24 p-8 bg-slate-500 text-slate-200'>
-            <div className=' text-4xl'>
-                <Link href="/">
-                    WorkWave
-                </Link>
-            </div>
+        <div className='flex justify-between items-center h-24 sticky top-0 p-8  bg-slate-500 text-slate-200'>
+
+            <Link href="/">
+                WorkWave
+            </Link>
+
+            <Link href="/board">Job Postings</Link>
             <Button />
         </div>
     )
